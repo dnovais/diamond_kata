3bfd11bfdb18a35b57953f7ae6636a5579df6a43
Diego Novais <contato@diegonovais.com.br>
2018-06-30 11:52:50 -0300

    Refactoring the code

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,62 +1,43 @@
 class Diamond
 	def build(input)
-		@input = input
-		special_char = "_"
-		break_point = "\n"
-		range = ("A"..input).to_a
-		range_size = range.size
-		output = []
-
-		# range.each_with_index do |char, index|
-			base_diamond = set_base(range_size, special_char)
-			draw_base = draw_base(base_diamond)
-			draw_diamond(range)
-		# end
-
+		@letters = ("A"..input).to_a
+		draw_diamond
 	end
 
-	def draw_diamond(range)
-		range_size = range.size
-		special_char = "_"
-		output = ""
+  def draw_diamond
+		output= ""
 
-		base = range_size.even? ? special_char * (range_size + 1) : special_char * (range_size + 2)
-		base_size = base.size
-
-		range.each_with_index do |char, index|
-			if range[index] == @input
-				base[0] = char
-				base[base_size - 1] = char
-				output += base + "\n"
-			else
-				# puts "#{range[index]} - #{index}"
-				# base[base_size - (range_size - index)] = range[index]
-				# output += base + "\n"
-			end
+		# For each char call draw_base.
+		@letters.each do |char| 
+			output << draw_base(char)
 		end
 
-
-		# range.reverse.each_with_index do |char, index|
-		# 	puts "#{char} #{index + 1} = #{range.reverse[index+1]}"
-		# end
-		
-		puts output
-		p output
-	end
-
-	def draw_base(base)
-		diamond_draw = ""
-		base_size = base.size
-		base_size.times do
-			diamond_draw += base + "\n"
+		# For each reverse char without first position call draw_base.
+		@letters.reverse[1..-1].each do |char| 
+			output << draw_base(char)
 		end
-		diamond_draw
+
+		output
 	end
 
-	def set_base(size, special_char)
-		size.even? ? special_char * (size + 1) : special_char * (size + 2) 
-	end
+  def draw_base (letter)
+		base = ""
+
+		# For each reverse char assign letter_or_underline to base.
+		@letters.reverse.each do |char| 
+			base << letter_or_underline(char, letter)
+		end
+
+		# For each char without first position assign letter_or_underline to base.
+		@letters[1..-1].each do |char| 
+			base << letter_or_underline(char, letter)
+		end
+
+		base << "\n"
+  end
+	
+	def letter_or_underline (char, letter)
+		# Return letter if char is equal letter
+    (char == letter) ? letter : '_'
+  end
 end
-
-d = Diamond.new
-d.build("C")
